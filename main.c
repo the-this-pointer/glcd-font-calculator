ec5490f137b4ce1cd445c7f660e65aecee2b315f
Reza Kiani <rkm7232@gmail.com>
2022-10-28 23:50:16 +0330

    better character editor and added copy to clipboard

--- main.c
+++ main.c
@@ -24,7 +24,7 @@
 
 static int selected[COL_W * COL_H] = {0};
 static uint16_t character[COL_H] = {
-  /*0x0000,
+  0x0000,
   0x0C00,
   0x0C00,
   0x0C00,
@@ -41,13 +41,11 @@ static uint16_t character[COL_H] = {
   0x0C00,
   0x0000,
   0x0000,
-  0x0000,*/
-    0x0000
+  0x0000,
 };
 
 void ConvertCharToBuffer();
-
-void ConvertBufferToChar();
+void ConvertBufferToChar(char* buffer);
 
 /* ===============================================================
  *
@@ -79,6 +77,15 @@ int main(void)
   HWND wnd;
   int running = 1;
   int needs_refresh = 1;
+  static struct nk_vec2 clicked_pos;
+  static int clicked_active = 0;
+  static int rclicked_active = 0;
+  static char text_val[256];
+  static int text_val_len;
+
+  HWND hWnd = GetConsoleWindow();
+  ShowWindow( hWnd, SW_MINIMIZE );  //won't hide the window without SW_MINIMIZE
+  ShowWindow( hWnd, SW_HIDE );
 
   /* Win32 */
   memset(&wc, 0, sizeof(wc));
@@ -130,23 +137,59 @@ int main(void)
     nk_input_end(ctx);
 
     /* GUI */
-    if (nk_begin(ctx, "Calculator", nk_rect(0, 0, WINDOW_WIDTH-250, WINDOW_HEIGHT), 0x00))
+    if (nk_begin(ctx, "Calculator", nk_rect(0, 0, WINDOW_WIDTH-240, WINDOW_HEIGHT), 0x00))
     {
-      int i;
-      nk_layout_row_static(ctx, CELL_SIZE, CELL_SIZE, 11);
-      for (i = 0; i < COL_W * COL_H; ++i) {
-        nk_checkbox_label(ctx, "", &selected[i]);
+      struct nk_command_buffer* out = nk_window_get_canvas(ctx);
+      struct nk_rect bounding_rect = nk_window_get_bounds(ctx);
+      uint16_t cell_size = (bounding_rect.h - 22) / COL_H;
+      struct nk_rect rects_bounding_rect = nk_rect(12, 16, cell_size * COL_W, cell_size * COL_H);
+      for (int i = 0; i < COL_H; ++i) {
+        for (int j = 0; j < COL_W; ++j) {
+          nk_fill_rect(out, nk_rect(12 + cell_size * j, 16 + cell_size * i, cell_size, cell_size), 0.0,
+                       selected[(i * COL_W) + j] == 0? nk_rgba(57, 67, 71, 215) : nk_rgba(195, 55, 75, 255));
+        }
+      }
+
+      if (nk_input_is_mouse_click_down_in_rect(&ctx->input, NK_BUTTON_LEFT,
+                                               rects_bounding_rect,nk_true)){
+        clicked_active = 1;
+      }
+      if (nk_input_is_mouse_click_down_in_rect(&ctx->input, NK_BUTTON_RIGHT,
+                                                    rects_bounding_rect,nk_true)){
+        rclicked_active = 1;
+      }
+      if (!nk_input_is_mouse_down(&ctx->input, NK_BUTTON_LEFT) && clicked_active) {
+        clicked_active = 0;
+      }
+      if (!nk_input_is_mouse_down(&ctx->input, NK_BUTTON_RIGHT) && rclicked_active) {
+        rclicked_active = 0;
+      }
+
+      if (clicked_active || rclicked_active) {
+        clicked_pos.x = ctx->input.mouse.pos.x - 12;
+        clicked_pos.y = ctx->input.mouse.pos.y - 16;
+        int x = ((float)COL_W * clicked_pos.x) / rects_bounding_rect.w;
+        int y = ((float)COL_H * clicked_pos.y) / rects_bounding_rect.h;
+
+        selected[y * COL_W + x] = clicked_active? 1: 0;
       }
     }
     nk_end(ctx);
 
-    if (nk_begin(ctx, "Value", nk_rect(WINDOW_WIDTH-250, 50, 200, 200),
+    if (nk_begin(ctx, "Value", nk_rect(WINDOW_WIDTH-245, 50, 230, 400),
                  NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
     {
+      nk_layout_row_static(ctx, 50, 200, 1);
+      nk_label_wrap(ctx, "Use left-click to set the box and right-click for resetting it!");
+
       nk_layout_row_static(ctx, 30, 80, 1);
-      if (nk_button_label(ctx, "Print value")) {
-        ConvertBufferToChar();
+      nk_label(ctx, "Value:", NK_TEXT_LEFT);
+      if (nk_button_label(ctx, "Calc & Copy")) {
+        ConvertBufferToChar(text_val);
+        text_val_len = strlen(text_val);
       }
+      nk_layout_row_static(ctx, 100, 200, 1);
+      nk_edit_string(ctx, NK_EDIT_BOX, text_val, &text_val_len, 256, nk_filter_default);
     }
     nk_end(ctx);
 
@@ -160,8 +203,10 @@ int main(void)
   return 0;
 }
 
-void ConvertBufferToChar() {
-  uint16_t i, b, j;
+void ConvertBufferToChar(char* buffer) {
+  uint16_t i, j, ptr = 0;
+  memset(buffer, 0, 256);
+
   for (i = 0; i < COL_H; i++)
   {
     character[i] = 0;
@@ -177,8 +222,18 @@ void ConvertBufferToChar() {
         character[i] &= ~((uint16_t)1 << bit);
       }
     }
-    printf("0x%04X, ", character[i]);
+    sprintf(buffer + ptr, "0x%04X, ", character[i]);
+    ptr += 8;
   }
+
+  const size_t len = strlen(buffer) + 1;
+  HGLOBAL hMem =  GlobalAlloc(GMEM_MOVEABLE, len);
+  memcpy(GlobalLock(hMem), buffer, len);
+  GlobalUnlock(hMem);
+  OpenClipboard(0);
+  EmptyClipboard();
+  SetClipboardData(CF_TEXT, hMem);
+  CloseClipboard();
 }
 
 void ConvertCharToBuffer() {
